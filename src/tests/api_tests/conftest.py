b8cb91c05dda2eb46ba53f21840cd3fdb166c6dd
Korionov Dmitriy <koriond98@gmail.com>
2024-07-21 19:22:10 +0300

    inital commit

---
+++ src/tests/api_tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from core.config import MainSettings
+from domain_tests.conftest import (  # noqa
+    provide_auth_controller,
+    provide_user_controller,
+)
+from httpx import AsyncClient
+from services.security import JwtAuthService
+
+
+@pytest.fixture(scope="module")
+def get_jwt_service(get_test_settings: MainSettings) -> JwtAuthService:
+    return JwtAuthService(get_test_settings.security)
+
+
+def login_client(client: AsyncClient, user_id: int, get_jwt_service: JwtAuthService):
+    tokens = get_jwt_service.generate_jwt_tokens(user_id)
+    client.headers.update({"Authorization": f"Bearer {tokens['access_token']}"})
+
+
+def logout_client(client: AsyncClient):
+    client.headers.pop("Authorization")
